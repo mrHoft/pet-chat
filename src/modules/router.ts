3a5067dda4b1754218f4bc86bcd18e7c0c1e9674
Hoft <mrhoft@yandex.ru>
2023-01-29 12:40:22 +0300

    x

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,13 +1,13 @@
 import * as routes from './routes_data';
 
 function resolveRoute(route:string) {
-	if(routes[route]) return routes[route];
+	if(routes[route as keyof typeof routes]) return routes[route as keyof typeof routes];
 	else return routes['no_page'];
 };
 
-function router(evt:Event):void {
-	// let url=window.location.hash.slice(1) || 'home';
-	let url:string=window.location.pathname.replace(/^\/|\/$/, '') || 'home';
+function router(evt:HashChangeEvent):void {
+	let url=window.location.hash.slice(1) || 'home';
+	// let url:string=window.location.pathname.replace(/^\/|\/$/, '') || 'home';
 	console.log(`Route: ${url}`);
 	let route=resolveRoute(url);
 	route();
